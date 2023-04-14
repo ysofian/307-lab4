3ef47777553a4669e329f4b15101ad6847de4ffe
ysofian <ysofian@calpoly.edu>
2023-04-14 13:55:44 -0700

    updated frontend

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import Table from './Table'
-
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
+import React, {useState} from 'react';
+import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
-    return (
-        <div className="container">
-        <Table characterData={characters} />
-        </div>
-    )
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
 }
 
 export default MyApp;
--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -1,41 +1,46 @@
 import React from 'react'
 
 function TableHeader()  {
-    return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
-    );
-  }
-  
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  );
+}
+
+
 function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.name}</td>         
-                <td>{row.job}</td>
-            </tr>
-        );
-    });
+  const rows = props.characterData.map((row, index) => {
     return (
-        <tbody>
-            {rows}
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
+        </tr>
+      );
+    }
+  );
+  return (
+      <tbody>
+        {rows}
         </tbody>
     );
 }
-  
-  
-function Table(props) {
-    return (
-        <table>
-            <TableHeader />
-            <TableBody characterData={props.characterData} />
-        </table>
-    );
+
+
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
-export default Table;
 
+export default Table;
